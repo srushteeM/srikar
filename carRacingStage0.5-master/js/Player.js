049e8fbb1de6114ca57dee32653f74365447b640
srushteeM <59560681+srushteeM@users.noreply.github.com>
2020-05-21 21:01:23 +0530

    Add files via upload

---
+++ carRacingStage0.5-master/js/Player.js
@@ -0,0 +1,33 @@
+class Player {
+  constructor(){
+    this.index=null;
+    this.name=name;
+    this.distance=0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+  static getPlayerInfo(){
+    database.ref("players").on("value",(data)=>{
+      allPlayers=data.val();
+    })
+  }
+}
